666bbaadac2056e71eec14632eec98c96e1a8133
abuXahra <abdulmuminisah@yahoo.com>
2026-07-25 00:02:45 +0100

    Authentication sysem backend

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDb = require("./config/connectDb");
+const errorMiddleware = require("./middleware/error.middleware");
+const userRoute = require("./routes/user.route");
+
+const app = express();
+dotenv.config();
+connectDb();
+
+app.use(express.json());
+app.use("/api/auth", userRoute);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
